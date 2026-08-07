52d15ea70d25d542dc879a7382b2b3318dd2f282
wools0700 <wools0700@gmail.com>
2026-08-07 11:43:20 +0900

    feature(worker): 카메라 영상 원본 수집 및 30프레임 당 이미지 수집

--- worker/camera_run.py
+++ worker/camera_run.py
@@ -1,7 +1,10 @@
+import time
 import cv2
 
 from camera import Camera
 from camera_stream import CameraStream
+from video_recorder import VideoRecorder
+from frame_capture import FrameCapture
 from config import CAMERA_NAME, RTSP_URL
 
 
@@ -12,6 +15,9 @@ stream = CameraStream(
 
 stream.start()
 
+# FFmpeg가 RTSP 스트림을 생성할 시간 확보
+time.sleep(3)
+
 
 camera = Camera(
     RTSP_URL
@@ -20,12 +26,38 @@ camera = Camera(
 camera.start()
 
 
+video_recorder = VideoRecorder(
+    save_path="data/video",
+    fps=30,
+    frame_size=(640,480)
+)
+
+video_recorder.start()
+
+
+frame_capture = FrameCapture(
+    save_path="data/frames",
+    interval=30
+)
+
+frame_capture.start()
+
+
+
 while True:
 
     frame = camera.read()
 
     if frame is None:
         break
+
+
+    # 원본 영상 저장
+    video_recorder.write(frame)
+
+
+    # 30프레임당 1장 저장
+    frame_capture.save(frame)
 
 
     cv2.imshow(
@@ -40,6 +72,11 @@ while True:
 
 
 camera.stop()
+
+video_recorder.stop()
+
+frame_capture.stop()
+
 stream.stop()
 
 cv2.destroyAllWindows()
